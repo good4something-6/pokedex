dd9891a57ef8e64546f2ec32c81b1506c8103a34
Hammam Alzilitni <hammam@Hammams-MacBook-Pro.local>
2022-05-31 14:03:06 +0100

    pushing before crash

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import './App.css';
-import {getPokemon} from "../api";
-import { useEffect } from 'react';
-import Pokedex from 'pokedex-promise-v2';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import Pokedex from "pokedex-promise-v2";
 
 function App() {
-  const P = new Pokedex();
+  const P = new Pokedex({ timeout: 1800 * 1000 });
+  const [pokemon, setPokemon] = useState([]);
 
-  
+  useEffect(() => {
+    // P.getGenerationByName("generation-i")
+    // .then((response) => {
+    //   setPokemon(response.pokemon_species);
+    // })
+    // .catch((error) => {
+    //   console.log('There was an ERROR: ', error);
+    // });
+    for (let i = 1; i < 152; i++)
+      [
+        P.getPokemonByName(i).then((response) => {
+          console.log("response", response);
+        }),
+      ];
+  }, []);
 
-
+  console.log("set of pokemon", pokemon);
 
   return (
     <div className="App">
-    
+      {pokemon.map((pokemon) => {
+        return <div>{pokemon.name}</div>;
+      })}
     </div>
   );
 }
