53ce53a7e69900c6dc9e9610859bca19d87b5646
Gareth Dawson <gwdawson.work@gmail.com>
2022-05-31 14:19:58 +0100

    fetch all gen 1 pokemon

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import Pokedex from "pokedex-promise-v2";
+
+function App() {
+  const P = new Pokedex({ timeout: 1800 * 1000 });
+  const [pokemon, setPokemon] = useState([]);
+
+  const makeAPIcall = async () => {
+    for (let i = 1; i < 152; i++) {
+      const response = await P.getPokemonByName(i);
+      setPokemon((pokemon) => [...pokemon, response]);
+    }
+  };
+
+  useEffect(async () => {
+    await makeAPIcall();
+  }, []);
+
+  useEffect(() => {
+    console.log("all", pokemon);
+  }, [pokemon]);
+
+  // console.log("set of pokemon", pokemon);
+
+  return (
+    <div className="App">
+      {pokemon.map((pokemon) => {
+        return (
+          <div className="container">
+            <h1>{pokemon.name}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
